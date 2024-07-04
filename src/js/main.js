fe3766f67bf3ad6e044b5dd3924c9eb066630d4b
Paulo Germano <>
2024-07-04 10:52:38 -0300

    adds new bootstrap install

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+//
+// Place any custom JS here
+//
+
+// Dark Mode Switch
+document.addEventListener('DOMContentLoaded', (event) => {
+  const htmlElement = document.documentElement;
+  const switchElement = document.getElementById('darkModeSwitch');
+
+  // Set the default theme to dark if no setting is found in local storage
+  const currentTheme = localStorage.getItem('bsTheme') || 'light';
+  htmlElement.setAttribute('data-bs-theme', currentTheme);
+  switchElement.checked = currentTheme === 'dark';
+
+  switchElement.addEventListener('change', function () {
+      if (this.checked) {
+          htmlElement.setAttribute('data-bs-theme', 'dark');
+          localStorage.setItem('bsTheme', 'dark');
+      } else {
+          htmlElement.setAttribute('data-bs-theme', 'light');
+          localStorage.setItem('bsTheme', 'light');
+      }
+  });
+});

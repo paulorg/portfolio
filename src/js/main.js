66e32fca94221e7485ecfd3732e63e8849c936b0
Paulo Germano <>
2024-07-04 20:10:42 -0300

    Fixes dark mode

--- src/js/main.js
+++ src/js/main.js
@@ -11,22 +11,31 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 // Dark Mode Switch
 document.addEventListener('DOMContentLoaded', (event) => {
-  const htmlElement = document.documentElement;
-  const switchElement = document.getElementById('darkModeSwitch');
+    const htmlElement = document.documentElement;
+    const switchElement = document.getElementById('darkModeSwitch');
 
-  // Set the default theme to dark if no setting is found in local storage
-  const currentTheme = localStorage.getItem('bsTheme') || 'light';
-  htmlElement.setAttribute('data-bs-theme', currentTheme);
-  switchElement.checked = currentTheme === 'dark';
+    // Set the default theme to dark if no setting is found in local storage
+    const currentTheme = localStorage.getItem('bsTheme') || 'light';
+    htmlElement.setAttribute('data-bs-theme', currentTheme);
+    switchElement.checked = currentTheme === 'dark';
 
-  switchElement.addEventListener('change', function () {
-      if (this.checked) {
-          htmlElement.setAttribute('data-bs-theme', 'dark');
-          localStorage.setItem('bsTheme', 'dark');
-      } else {
-          htmlElement.setAttribute('data-bs-theme', 'light');
-          localStorage.setItem('bsTheme', 'light');
-      }
-  });
+    switchElement.addEventListener('change', function () {
+        if (this.checked) {
+            htmlElement.setAttribute('data-bs-theme', 'dark');
+            localStorage.setItem('bsTheme', 'dark');
+        } else {
+            htmlElement.setAttribute('data-bs-theme', 'light');
+            localStorage.setItem('bsTheme', 'light');
+        }
+    });
+
+
+    const matchPrefersLight = window.matchMedia('(prefers-color-scheme:light)');
+    if (matchPrefersLight.matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'light');
+    }
+    matchPrefersLight.addEventListener('change', event => {
+        document.documentElement.setAttribute('data-bs-theme', event.matches ? "light" : "dark");
+    });
+
 });
-
